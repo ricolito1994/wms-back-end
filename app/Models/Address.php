1623ffd5d526005371ddb9aeb014c74d530b5c98
Ricolito Cantorias <ricomagnifico018@gmail.com>
2025-02-03 00:20:44 +0800

    wms-3-be wip

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -72,14 +73,12 @@ class Address extends Model
             $query->where('barangay_id', $barangay_id);
         })->when($request->purok_id, function($query, $purok_id) {
             $query->where('purok_id', $purok_id);
-        })->when($request->city_id, function($query, $city_id) {
-            $query->where('city_id', $city_id);
         })->when($request->address_type , function($query, $address_type) {
             $query->where('address_type', $address_type);
         })->when($request->address_name, function($query, $address_name) {
             $query->where (DB::raw("CONCAT(lot_number, ' ', street)"), 'LIKE', "%{$address_name}%");
-        })->when($request->full_address_name, function($query, $full_address_name) {
-            $query->where (DB::raw("full_address"), 'LIKE', "%{$full_address_name}%");
+        })->when($request->full_address, function($query, $full_address) {
+            $query->where (DB::raw("full_address"), 'LIKE', "%{$full_address}%");
         });
     }
 
