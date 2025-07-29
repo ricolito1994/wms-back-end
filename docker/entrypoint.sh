49ebf4b8ab30ea682236a2fe91973923c16c9ad7
Ricolito Cantorias <ricomagnifico018@gmail.com>
2025-07-30 01:15:54 +0800

    aws ec2 setup switch back to phpfpm

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,17 +5,52 @@ cd /var/www
 
 echo "⚙️  Environment: $APP_ENV"
 
-# Copy environment file if it doesn't exist
+echo "COPYING php.ini ...";
 if [ "$APP_ENV" = "production" ]; then
-  cp -n .env.production.example .env
+    cp /usr/local/etc/php/php.prod.ini /usr/local/etc/php/php.ini
 else
-  cp -n .env.development.example .env
+    cp /usr/local/etc/php/php.dev.ini /usr/local/etc/php/php.ini
 fi
+
+echo "COPYING .env ...";
+# Detect env
+if [ "$APP_ENV" = "production" ]; then
+    ENV_FILE="/var/www/.env.prod.example"
+else
+    ENV_FILE="/var/www/.env.dev.example"
+fi
+
+# 1️⃣ Overwrite .env from correct example file
+if [ -f "$ENV_FILE" ]; then
+    echo "📝 Using $ENV_FILE → .env"
+    cp -f "$ENV_FILE" /var/www/.env
+else
+    echo "⚠️ $ENV_FILE not found!"
+fi
+
+MAX_TRIES = 3
+NUM_TRIES = 0
+
+# Extract DB_HOST and DB_PORT from .env
+DB_HOST=$(grep DB_HOST /var/www/.env | cut -d '=' -f2)
+DB_PORT=$(grep DB_PORT /var/www/.env | cut -d '=' -f2)
+
+echo "⏳ Waiting for database ($DB_HOST:$DB_PORT) to be ready..."
+until nc -z "$DB_HOST" "$DB_PORT"; do
+    NUM_TRIES=$((NUM_TRIES+1))
+    if [ "$TRIES" -ge "$MAX_TRIES" ]; then
+        echo "❌ Could not connect to database after $MAX_TRIES attempts, exiting..."
+        exit 1
+    fi
+    echo "   ⏳ DB not ready attempt $NUM_TRIES/$MAX_TRIES..., retrying in 3s..."
+    sleep 3
+done
+echo "✅ Database is ready!"
 
 # Ensure correct permissions for storage and cache
 echo "🔧 Setting permissions..."
-chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
+chown -R wms:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R ug+rwx /var/www/storage /var/www/bootstrap/cache
 
 # Install optimized dependencies
 echo "📦 Installing Composer dependencies..."
@@ -36,5 +71,6 @@ php artisan migrate --force
 # Optional: start cron if needed (usually handled via Supervisor)
 # service cron start
 
-echo "✅ Entrypoint completed. Starting supervisord..."
-exec "$@"
+echo "✅ [wms-entrypoint] Init complete, starting Supervisor..."
+exec /usr/bin/supervisord -c /etc/supervisord.conf
+# exec "$@"
